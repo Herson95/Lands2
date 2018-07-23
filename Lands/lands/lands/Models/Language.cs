7bbc2d723ebdc79ab8e3edfbd3afb1b9efc287ab
Centro de Desarrollo De Software <Herson95>
2018-07-23 09:33:28 -0600

    ApiService

---
+++ Lands/lands/lands/Models/Language.cs
@@ -0,0 +1,22 @@
+﻿namespace lands.Models
+{
+    using Newtonsoft.Json;
+
+    public class Language
+    {
+        public class Currency
+        {
+            [JsonProperty(PropertyName = "iso639_1")]
+            public string Iso6391 { get; set; }
+
+            [JsonProperty(PropertyName = "iso639_2")]
+            public string Iso6392 { get; set; }
+
+            [JsonProperty(PropertyName = "name")]
+            public string Name { get; set; }
+
+            [JsonProperty(PropertyName = "nativeName")]
+            public string NativeName { get; set; }
+        }
+    }
+}
