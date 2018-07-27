3dfd87d2f6377327ce71d1a0e215f84a7ad0a53a
Centro de Desarrollo De Software <>
2018-07-27 09:36:25 -0600

    Language Page

--- Lands/lands/lands/Models/Language.cs
+++ Lands/lands/lands/Models/Language.cs
@@ -4,19 +4,18 @@
 
     public class Language
     {
-        public class Currency
-        {
-            [JsonProperty(PropertyName = "iso639_1")]
-            public string Iso6391 { get; set; }
+        
+        [JsonProperty(PropertyName = "iso639_1")]
+        public string Iso6391 { get; set; }
 
-            [JsonProperty(PropertyName = "iso639_2")]
-            public string Iso6392 { get; set; }
+        [JsonProperty(PropertyName = "iso639_2")]
+        public string Iso6392 { get; set; }
 
-            [JsonProperty(PropertyName = "name")]
-            public string Name { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
 
-            [JsonProperty(PropertyName = "nativeName")]
-            public string NativeName { get; set; }
-        }
+        [JsonProperty(PropertyName = "nativeName")]
+        public string NativeName { get; set; }
+       
     }
 }
--- Lands/lands/lands/ViewModels/LandViewModel.cs
+++ Lands/lands/lands/ViewModels/LandViewModel.cs
@@ -11,6 +11,8 @@ namespace lands.ViewModels
     {
         #region Attributes
         private ObservableCollection<Border> borders;
+        private ObservableCollection<Currency> currencies;
+        private ObservableCollection<Language> languages;
         #endregion
 
         #region Properties
@@ -31,6 +33,30 @@ namespace lands.ViewModels
                 this.SetValue(ref this.borders, value);
             }
         }
+
+        public ObservableCollection<Currency> Currencies
+        {
+            get
+            {
+                return this.currencies;
+            }
+            set
+            {
+                this.SetValue(ref this.currencies, value);
+            }
+        }
+
+        public ObservableCollection<Language> Languages 
+        {
+            get
+            {
+                return this.languages;
+            }
+            set
+            {
+                this.SetValue(ref this.languages, value);
+            }
+        }
         #endregion
 
         #region Constructors
@@ -38,6 +64,8 @@ namespace lands.ViewModels
         {
             this.Land = land;
             this.LoadBorders();
+            this.Currencies = new ObservableCollection<Currency>(this.Land.Currencies);
+            this.Languages = new ObservableCollection<Language>(this.Land.Languages);
         }
         #endregion
 
