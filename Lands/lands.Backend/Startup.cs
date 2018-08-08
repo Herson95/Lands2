6dfd25f4953daf01c4bf00923ebdf21d41886d94
Herson95 <38219534+Herson95@users.noreply.github.com>
2018-08-08 14:29:14 -0500

    API

---
+++ Lands/lands.Backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lands.Backend.Startup))]
+namespace lands.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
